bffdc2d4c7981c4906d14cac0ba38a22613e3990
enavid <en.navid@outlook.com>
2021-04-28 14:47:35 +0430

    Try to add element. I should change this code and use li tag

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,23 @@
 const todo = [];
-const addButton = document.getElementById('addButton');
-const textField = document.getElementById('textField')
+const get = document.getElementById.bind(document);
+const create = document.createElement.bind(document);
+const addButton = get('addButton');
+const textField = get('textField');
+const section = get('test');
+//var create = document.createElement.bind(document);
 
-addButton.addEventListener("click", () => {
-    let value = textField.value;
-    alert(value);
+
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const div = create('div');
+    const p = create('p');
+    const value = textField.value;
+
+    div.setAttribute("class", "todos");
+    p.innerText = value;
+    div.appendChild(p);
+
+    section.prevent(div);
+
 })

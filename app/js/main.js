df23214f6bbaae8ee385bd9e2aa62ff58e911ceb
enavid <en.navid@outlook.com>
2021-04-29 17:46:26 +0430

    add li and ul tag to code

--- app/js/main.js
+++ app/js/main.js
@@ -3,21 +3,16 @@ const get = document.getElementById.bind(document);
 const create = document.createElement.bind(document);
 const addButton = get('addButton');
 const textField = get('textField');
-const section = get('test');
-//var create = document.createElement.bind(document);
-
+const ul = get('ul');
 
 addButton.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const div = create('div');
-    const p = create('p');
+    const li = create('li');
     const value = textField.value;
 
-    div.setAttribute("class", "todos");
-    p.innerText = value;
-    div.appendChild(p);
-
-    section.prevent(div);
+    li.innerHTML = value;
+    ul.appendChild(li);
+    ul.appendChild(div);
 
 })

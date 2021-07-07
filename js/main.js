47bb104a12cb4aedc6e39fa2370f040e9db656b1
enavid <en.navid@outlook.com>
2021-07-07 21:45:57 +0430

    add trash and checkbox item

--- js/main.js
+++ js/main.js
@@ -10,15 +10,25 @@ addButton.addEventListener("click", (e) => {
 
     const li = create('li');
     const p = create('p');
-    const trash = create('img')
+    const trash = create('img');
+    const span1 = create('span');
+    const span2 = create('span');
+    const input = create('input');
     const value = textField.value;
 
+    span1.setAttribute('id', 'ul-1');
+    input.setAttribute('type', 'checkbox');
+    span1.appendChild(input);
+    p.innerHTML = value;
+    span1.appendChild(p);
+    li.appendChild(span1);
+
+    span2.setAttribute('id', 'ul-2');
     trash.setAttribute('src', './public/icon/trash.png');
     trash.setAttribute('alt', 'trash');
+    span2.appendChild(trash);
+    li.appendChild(span2);
 
-    p.innerHTML = value;
-    li.appendChild(p);
-    li.appendChild(trash);
     ul.prepend(li);
     textField.value = '';
 

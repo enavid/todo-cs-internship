28dc35fd019f51461eeef0b1cd79bbc5c7fe7b46
enavid <en.navid@outlook.com>
2021-08-07 19:53:26 +0430

    remove model function and add to controller

--- js/controller.js
+++ js/controller.js
@@ -12,26 +12,26 @@ _view.addEventListener((e) => {
         if (checkIterative(e.todo.value)) {
             alert(e.todo.value + ' Item is exist !');
         } else {
-            _model.addTodo(e.todo);
+            _model.push(e.todo)
             _view.renderSingleItem(e.todo);
         }
     };
 
     if (event === 'checkBox') return toggleComplete(e.todo);
 
-    if (event === 'trash') return removeItem(e.todo), _view.render(_model.getTodos());
+    if (event === 'trash') return removeItem(e.todo), _view.render(_model);
 
     if (event === 'check') {
         if (e.inputEdit.value === '') return alert('Please enter valid input!');
         updateItem(e.todo, e.inputEdit.value);
-        _view.render(_model.getTodos())
+        _view.render(_model)
     }
 
-    if (event === 'All') return _view.render(filterItem(event, _model.getTodos()));
+    if (event === 'All') return _view.render(filterItem(event, _model));
 
-    if (event === 'Active') return _view.render(filterItem(event, _model.getTodos()));
+    if (event === 'Active') return _view.render(filterItem(event, _model));
 
-    if (event === 'Complete') return _view.render(filterItem(event, _model.getTodos()));
+    if (event === 'Complete') return _view.render(filterItem(event, _model));
 
 });
 
@@ -45,15 +45,16 @@ function updateItem(todo, update) {
 }
 
 function checkIterative(value) {
-    _model.getTodos().find((data) => data.value === value);
+    _model.find((data) => data.value === value);
 }
 
 function removeItem(todo) {
-    _model.removeItem(todo);
+    _model.splice(_todos.indexOf(todo), 1);
 }
 
 function filterItem(state, todos) {
     return state === 'All' ? todos :
         state === 'Active' ? todos.filter(item => !item.complete) : todos.filter(item => item.complete);
 }
+
 
--- js/model.js
+++ js/model.js
@@ -2,17 +2,6 @@ const _todos = [];
 
 //================================ Model API ===================================
 
-export default { addTodo, getTodos, removeItem };
+export default _todos;
 
-// =============================== Define control function =====================
-function addTodo(todo) {
-    _todos.push(todo)
-};
 
-function getTodos() {
-    return _todos
-};
-
-function removeItem(todo) {
-    _todos.splice(_todos.indexOf(todo), 1);
-}

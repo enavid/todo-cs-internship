85c79c1206fc8838e7229929329cb866d131616a
enavid <en.navid@outlook.com>
2021-11-19 15:43:45 +0330

    Add getTodos root to routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const controller = require('./controller');
 const routes = require('./utils/routes');
+const _model = require('./model');
 
 const server = http.createServer();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 
 server.on('request', (req, res) => {
     routes.static(controller.staticFiles, { req, res });
+    routes.get('/todos', controller.getTodos, { req, res });
 })
 
 server.listen(PORT, () => {

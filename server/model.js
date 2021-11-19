a102ef6b064af41cc57d0a7f59ca60b6e958861a
enavid <en.navid@outlook.com>
2021-11-19 15:41:07 +0330

    Add model.js to servers

---
+++ server/model.js
@@ -0,0 +1,6 @@
+const _todos = [
+    { value: 'reading books 1', complete: false },
+    { value: 'reading books 2', complete: false },
+    { value: 'reading books 3', complete: false },
+    { value: 'reading books 4', complete: false },
+];

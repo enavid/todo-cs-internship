39d0f3d7630a55f3c5a965b17a31909250fa39ae
enavid <en.navid@outlook.com>
2022-01-14 16:55:55 +0330

    .

--- server/todo/views.py
+++ server/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def todo(request):
+    return render(request, 'index.html')

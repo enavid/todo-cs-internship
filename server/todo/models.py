4053009f823629965c2f42cc37c5f351eb7281c9
enavid <en.navid@outlook.com>
2022-01-14 17:19:36 +0330

    create database

--- server/todo/models.py
+++ server/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    id = models.AutoField(db_column='Id', primary_key=True)
+    todo = models.JSONField(db_column='todo')

f0d4ed717e0cf2604d8dd2b2ff215360946fc397
enavid <en.navid@outlook.com>
2022-01-14 17:14:28 +0330

    create todo API class

--- server/todo/urls.py
+++ server/todo/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-
     path('', views.todo, name='todo'),
-
+    path('todos/',  views.TodoAPI.as_view(), name='TodoAPI'),
 ]
--- server/todo/views.py
+++ server/todo/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 
 def todo(request):
     return render(request, 'index.html')
+
+
+class TodoAPI(APIView):
+
+    def get(self, request):
+        return Response('navids asdeghi', status=status.HTTP_200_OK)
+
+    def post(self, request):
+        pass

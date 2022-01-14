c132bd2a58ad04d1df9c1c8efedbbc828f4cbf41
enavid <en.navid@outlook.com>
2022-01-14 16:54:30 +0330

    Add static file.

---
+++ server/todo/static/js/controller.js
@@ -0,0 +1,93 @@
+import _model from './model.js'
+import _view from './view.js'
+
+//=========================== control event listener ====================
+
+_view.addEventListener('addButton', (todo) => {
+    if (todo.value === '') return alert('Please enter valid input!');
+    if (checkIterative(todo.value)) return alert(todo.value + ' Item is exist !');
+    _model.push(todo);
+    writeToLocalStorage(_model);
+    _view.renderSingleItem(todo);
+});
+
+_view.addEventListener('trash', (todo) => {
+    _model.splice(_model.indexOf(todo), 1);
+    writeToLocalStorage(_model);
+    _view.render(_model);
+});
+
+_view.addEventListener('checkBox', (todo) => {
+    todo.complete = !todo.complete;
+    writeToLocalStorage(_model);
+    _view.render(_model);
+});
+
+_view.addEventListener('check', (data) => {
+    if (data.update === '') return alert('Please enter valid input!');
+    checkIterative(data.update) ? alert(data.update + ' item is exist !') : data.todo.value = data.update;
+    writeToLocalStorage(_model);
+    _view.render(_model);
+});
+
+_view.addEventListener('allButton', () => _view.render(filterItem('All', _model)));
+
+_view.addEventListener('activeButton', () => _view.render(filterItem('Active', _model)));
+
+_view.addEventListener('completeButton', () => _view.render(filterItem('Complete', _model)));
+
+_view.addEventListener('download', () => {
+
+    const result = confirm('Download data ?');
+    if (result) {
+        fetch('/todos')
+            .then(response => response.json())
+            .then(data => {
+
+                const result = data.filter((item) => {
+                    return _model.includes(item)
+                })
+                if (result.length > 0) {
+                    console.log(result)
+                    _model.push(result);
+                    writeToLocalStorage(result);
+                }
+
+                _view.render(_model);
+            });
+    }
+})
+
+_view.addEventListener('upload', () => {
+    const result = confirm('Upload data ?');
+    if (result) {
+        fetch('/todos', {
+            method: 'POST',
+            body: JSON.stringify(_model),
+            headers: { 'Content-Type': 'application/json', },
+        })
+            .then(response => console.log(response))
+    }
+})
+// =============================== Define control function =====================
+
+function checkIterative(value) {
+    return _model.find((data) => data.value === value);
+}
+
+function filterItem(state, todos) {
+    return state === 'All' ? todos :
+        state === 'Active' ? todos.filter(item => !item.complete) : todos.filter(item => item.complete);
+}
+
+function writeToLocalStorage(model) {
+    localStorage.setItem('model', JSON.stringify(model));
+}
+
+(function checkLocalStorage() {
+    const model = JSON.parse(localStorage.getItem('model'));
+    if (model) {
+        _model.push(...model);
+    }
+})()
+
---
+++ server/todo/static/js/view.js
@@ -0,0 +1,136 @@
+const _get = document.getElementById.bind(document);
+const _addButton = _get('addButton');
+const _textField = _get('textField');
+const _list = _get('list');
+const _buttons = _get('buttons');
+const _eventHandler = {};
+
+//================================ view API ===========================
+
+export default { render, renderSingleItem, addEventListener };
+
+//=========================== view event listener ====================
+_addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const value = { "value": _textField.value, "complete": false };
+    _textField.value = '';
+    _textField.focus();
+    if (_eventHandler['addButton']) _eventHandler['addButton'](value);
+})
+
+_buttons.addEventListener('click', (e) => {
+    e.preventDefault();
+    const event = e.target.getAttribute('value');
+    if (event === 'All' && _eventHandler['allButton']) _eventHandler['allButton'](e);
+    if (event === 'Active' && _eventHandler['activeButton']) _eventHandler['activeButton'](e);
+    if (event === 'Complete' && _eventHandler['completeButton']) _eventHandler['completeButton'](e);
+    if (event === 'Upload' && _eventHandler['upload']) _eventHandler['upload']();
+    if (event === 'Download' && _eventHandler['download']) _eventHandler['download']();
+})
+
+//=========================== Define view function ===================
+
+function addEventListener(option, callBack) {
+    _eventHandler[option] = callBack;
+}
+
+function render(todos) {
+    _list.innerHTML = ' ';
+    todos.forEach(element => _list.prepend(creatItem(element)));
+}
+
+function renderSingleItem(todo) {
+    _list.prepend(creatItem(todo))
+}
+
+function renderEditInput(item) {
+    [...item.children].forEach(element => {
+        element.style.display = element.style.display === 'none' ? 'flex' : 'none';
+    })
+}
+
+function creatItem(todo) {
+
+    const create = document.createElement.bind(document);
+    const li = create('li');
+
+    // create input section 
+    const todoText = create('p');
+    const trash = create('img');
+    const span1 = create('span');
+    const span2 = create('span');
+    const checkbox = create('input');
+    const penEditButton = create('i');
+    const getInput = create('span');
+
+    span1.setAttribute('id', 'ul-1');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('value', 'checkBox');
+    checkbox.checked = todo.complete;
+    checkbox.addEventListener('click', () => {
+        if (_eventHandler['checkBox']) _eventHandler['checkBox'](todo);
+    });
+
+    span1.appendChild(checkbox);
+    todoText.innerHTML = todo.value;
+    todoText.className = todo.complete ? 'complete' : 'incomplete';
+    span1.appendChild(todoText);
+    getInput.appendChild(span1);
+    getInput.setAttribute('id', 'getInput');
+
+    span2.setAttribute('id', 'ul-2');
+    trash.setAttribute('src', '../static/public/icon/trash.png');
+    trash.setAttribute('alt', 'trash');
+    trash.setAttribute('value', 'trash');
+    trash.addEventListener('click', () => {
+        if (_eventHandler['trash']) _eventHandler['trash'](todo);
+    })
+
+    penEditButton.className = "fa fa-pencil";
+    penEditButton.setAttribute('value', 'penEdit');
+    penEditButton.addEventListener('click', () => {
+        renderEditInput(li);
+    })
+
+    span2.appendChild(penEditButton);
+    span2.appendChild(trash);
+    getInput.appendChild(span2);
+
+    // create edit section
+    const inputEdit = create('input');
+    const checkEditButton = create('p');
+    const closeEditButton = create('p');
+    const confirmSpan = create('span');
+    const editSpan = create('span');
+
+    editSpan.id = 'edit';
+    checkEditButton.innerHTML = "&#10004;";
+    checkEditButton.setAttribute('value', 'check');
+    checkEditButton.addEventListener('click', () => {
+        const event = { todo, 'update': inputEdit.value };
+        if (_eventHandler['check']) _eventHandler['check'](event);
+    })
+
+    closeEditButton.innerHTML = "&#10008;";
+    closeEditButton.setAttribute('value', 'close');
+    closeEditButton.addEventListener('click', () => {
+        renderEditInput(li);
+    })
+
+    confirmSpan.appendChild(closeEditButton);
+    confirmSpan.appendChild(checkEditButton);
+    confirmSpan.id = 'confirmEdit';
+
+    inputEdit.className = 'inputEdit';
+    inputEdit.value = todo.value;
+
+    editSpan.appendChild(inputEdit);
+    editSpan.appendChild(confirmSpan);
+    editSpan.style.display = 'none';
+
+    li.appendChild(editSpan);
+    li.appendChild(getInput);
+
+    //================================ Module AIP===========================
+    return li;
+}
